2ddc65249c694a6dedd6434b9322c6644ebc1e7a
Erdem <erdem@Erdems-MacBook-Pro-4.local>
2017-03-29 02:17:01 +0300

    second commit

--- Test/BinarySearchTreeFinderTests.cs
+++ Test/BinarySearchTreeFinderTests.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
 namespace Test
 {
-    public class Tests
+    public class BinarySearchTreeFinderTests
     {
         [Theory]
         [MemberData("DataBinary")]
-        public void Test1(int[] values, int from, int to, int expected)
+        public void given_input_values_when_finding_distnce_between_two_number_then_result_should_be(int[] values, int from, int to, int expected)
         {
             BinarySearchTreeFinder treeFinder = new BinarySearchTreeFinder();
             int distance = treeFinder.FindDistance(values, from, to);
@@ -20,7 +19,11 @@ namespace Test
         {
             return new List<object[]>
             {
-                new object[] {new object[] {5, 6, 3, 1, 2, 4}, 6, 2, 4}
+                new object[] {new object[] {5, 6, 3, 1, 2, 4}, 2, 4, 3},
+                new object[] {new object[] {5, 6, 3, 1, 2, 4}, 1, 4, 2},
+                new object[] {new object[] {5, 6, 3, 1, 2, 4}, 10, 4, -1},
+                new object[] {new object[] {5, 6, 3, 1, 2, 4}, 2, 10, -1},
+                new object[] {new object[] {5, 6, 3, 1, 2, 4, 8}, 2, 8, 5},
             };
         }
     }
@@ -56,20 +59,20 @@ namespace Test
 
         public int Distance(int from, int to)
         {
-//            int levelFrom = this.root.Search(@from);
-//            int levelTo = this.root.Search(to);
-//
-//            if (levelFrom == -1 || levelTo == -1)
-//            {
-//                return -1;
-//            }
+            int levelFrom = this.root.Search(@from);
+            int levelTo = this.root.Search(to);
+
+            if (levelFrom == -1 || levelTo == -1)
+            {
+                return -1;
+            }
             Node node = this.root.FindCommonNode(from, to);
             if (node == null)
             {
                 return -1;
             }
-            int distanceFrom = node.Distance(0,from);
-            int distanceTo = node.Distance(0,to);
+            int distanceFrom = node.Distance(0, from);
+            int distanceTo = node.Distance(0, to);
 
             return distanceFrom + distanceTo;
         }
@@ -114,33 +117,45 @@ namespace Test
 
         public Node FindCommonNode(int from, int to)
         {
-            if (this.value < from && this.value<to)
+            if (this.value >= from && this.value >= to)
             {
                 return this.left.FindCommonNode(from, to);
             }
-            if(this.value>= from&&this.value>=to)
+            if (this.value < from && this.value < to)
             {
                 return this.right.FindCommonNode(from, to);
             }
             return this;
         }
 
-        public int Distance(int count ,int from)
+        public int Distance(int count, int from)
         {
-
-            if (this.value==from)
+            if (this.value == from)
             {
                 return count;
             }
-            if (this.value<from)
+            if (this.value > from)
             {
+                if (this.left == null)
+                {
+                    return -1;
+                }
                 return this.left.Distance(count + 1, from);
             }
-            if (this.value>from)
+            if (this.value < from)
             {
+                if (this.right == null)
+                {
+                    return -1;
+                }
                 return this.right.Distance(count + 1, from);
             }
-            return 0;
+            return -1;
+        }
+
+        public int Search(int @from)
+        {
+            return this.Distance(0, from);
         }
     }
 }
